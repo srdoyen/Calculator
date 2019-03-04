bd2bac12b6c1388e1afbea8aaa38a0f99b8bc2e9
srdoyen <srdoyen@gmail.com>
2019-03-03 17:10:48 -0800

    Input complete

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,12 +1,22 @@
 import React, { Component } from "react";
 import "./App.css";
-import Button from "./components/Button";
+import { Button } from "./components/Button";
+import { Input } from "./components/Input";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      input: ""
+    };
+  }
+
   render() {
     return (
       <div className="App">
         <div className="calc-wrapper">
+          <Input input={this.state.input} />
           <div className="row">
             <Button>7</Button>
             <Button>8</Button>
