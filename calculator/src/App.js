240f5831f1367e6d9b39e4e9719f25073f9316a4
srdoyen <srdoyen@gmail.com>
2019-03-03 17:02:35 -0800

    Layout of Calc done

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -13,6 +13,24 @@ class App extends Component {
             <Button>9</Button>
             <Button>/</Button>
           </div>
+          <div className="row">
+            <Button>4</Button>
+            <Button>5</Button>
+            <Button>6</Button>
+            <Button>x</Button>
+          </div>
+          <div className="row">
+            <Button>1</Button>
+            <Button>2</Button>
+            <Button>3</Button>
+            <Button>+</Button>
+          </div>
+          <div className="row">
+            <Button>.</Button>
+            <Button>0</Button>
+            <Button>=</Button>
+            <Button>-</Button>
+          </div>
         </div>
       </div>
     );
